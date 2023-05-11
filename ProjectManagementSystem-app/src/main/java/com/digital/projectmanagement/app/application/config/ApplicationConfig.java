21d737671b4e7eeff28aacc5983fabf49eac765d
Tural Guluev <tural-98@bk.ru>
2023-05-11 14:42:26 +0300

    added main method in the ApplicationConfig class

--- ProjectManagementSystem-app/src/main/java/com/digital/projectmanagement/app/application/config/ApplicationConfig.java
+++ ProjectManagementSystem-app/src/main/java/com/digital/projectmanagement/app/application/config/ApplicationConfig.java
@@ -1,4 +1,7 @@
 package com.digital.projectmanagement.app.application.config;
 
 public class ApplicationConfig {
+    public static void main(String[] args) {
+        
+    }
 }
--- ProjectManagementSystem-business/ProjectManagementSystem-service/src/main/java/com/digital/projectmanagement/service/serviceImpl/AuthServiceImpl.java
+++ ProjectManagementSystem-business/ProjectManagementSystem-service/src/main/java/com/digital/projectmanagement/service/serviceImpl/AuthServiceImpl.java
@@ -1,6 +1,6 @@
 package com.digital.projectmanagement.service.serviceImpl;
 
-import com.digital.ProjectManagementSystem.service.AuthService;
+import com.digital.projectmanagement.service.AuthService;
 
 public class AuthServiceImpl implements AuthService {
 }
