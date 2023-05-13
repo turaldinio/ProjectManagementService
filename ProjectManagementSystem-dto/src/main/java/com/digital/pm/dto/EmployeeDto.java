bf62fd2da9ec17aec7fe4cbc0363dc231fe1e9ff
Tural Guluev <tural-98@bk.ru>
2023-05-13 19:30:37 +0300

    added fileds in EmployeeDto class

--- ProjectManagementSystem-dto/src/main/java/com/digital/pm/dto/EmployeeDto.java
+++ ProjectManagementSystem-dto/src/main/java/com/digital/pm/dto/EmployeeDto.java
@@ -1,4 +1,12 @@
 package com.digital.pm.dto;
 
+import com.digital.pm.common.enums.EmployeeStatus;
+
 public class EmployeeDto {
+    private int id;
+    private String fullName;
+    private String post;
+    private String account;
+    private String email;
+    private EmployeeStatus status;
 }
