bb2d1e1bf542d1eddb3fd6a676444e2172d316a6
Tural Guluev <tural-98@bk.ru>
2023-05-13 17:02:14 +0300

    added fileds in the  Team class and added Fileds to Roles enum

--- ProjectManagementSystem-business/ProjectManagementSystem-model/src/main/java/pm/model/Team.java
+++ ProjectManagementSystem-business/ProjectManagementSystem-model/src/main/java/pm/model/Team.java
@@ -1,4 +1,13 @@
 package pm.model;
 
+import com.digital.pm.common.enums.Roles;
+
+import java.util.Map;
+
 public class Team {
+    private Map<Employee, Roles> employees;
+
+    private Project project;
+
+
 }
--- ProjectManagementSystem-common/src/main/java/com/digital/pm/common/enums/Roles.java
+++ ProjectManagementSystem-common/src/main/java/com/digital/pm/common/enums/Roles.java
@@ -1,4 +1,8 @@
 package com.digital.pm.common.enums;
 
 public enum Roles {
+    MANAGER,
+    ANALYST,
+    DEVELOPER,
+    TESTER
 }
