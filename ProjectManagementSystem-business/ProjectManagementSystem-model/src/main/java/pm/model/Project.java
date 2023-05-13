e00e899334362fdfe83c23b2745a9bcc92865492
Tural Guluev <tural-98@bk.ru>
2023-05-13 15:34:23 +0300

    added fields in Project class

--- ProjectManagementSystem-business/ProjectManagementSystem-model/src/main/java/pm/model/Project.java
+++ ProjectManagementSystem-business/ProjectManagementSystem-model/src/main/java/pm/model/Project.java
@@ -1,4 +1,11 @@
 package pm.model;
 
+import com.digital.pm.common.enums.ProjectStatus;
+
 public class Project {
+    private int id;
+    private String name;
+    private String description;
+    private ProjectStatus status;
+
 }
