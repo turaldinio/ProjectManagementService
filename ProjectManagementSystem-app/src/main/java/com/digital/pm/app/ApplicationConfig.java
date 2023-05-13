3ddb195da47faeda8b1e36e14cdcfe929b416d39
Tural Guluev <tural-98@bk.ru>
2023-05-13 19:03:43 +0300

    added dto dependency in web pom

--- ProjectManagementSystem-app/src/main/java/com/digital/pm/app/ApplicationConfig.java
+++ ProjectManagementSystem-app/src/main/java/com/digital/pm/app/ApplicationConfig.java
@@ -1,9 +1,10 @@
 package com.digital.pm.app;
 
+import com.digital.pm.service.Impl.EmployeeServiceImpl;
 import com.digital.pm.web.controller.EmployeeController;
 
 public class ApplicationConfig {
-    private final EmployeeController employeeController = new EmployeeController();
+    private final EmployeeController employeeController = new EmployeeController(new EmployeeServiceImpl());
 
     public static void main(String[] args) {
 
--- ProjectManagementSystem-web/src/main/java/com/digital/pm/web/controller/EmployeeController.java
+++ ProjectManagementSystem-web/src/main/java/com/digital/pm/web/controller/EmployeeController.java
@@ -9,4 +9,6 @@ public class EmployeeController {
         this.employeeService=employeeService;
 
     }
+
+
 }
