42388ea5babf904380ad07a25d9ef87664180d18
Tural Guluev <tural-98@bk.ru>
2023-05-13 18:59:23 +0300

    created the DataStorageImpl

---
+++ ProjectManagementSystem-business/ProjectManagementSystem-repository/src/main/java/com/digital/pm/repository/impl/DataStorageImpl.java
@@ -0,0 +1,33 @@
+package com.digital.pm.repository.impl;
+
+import com.digital.pm.dto.EmployeeDto;
+import com.digital.pm.repository.DataStorage;
+
+import java.util.List;
+
+public class DataStorageImpl implements DataStorage {
+    @Override
+    public EmployeeDto create() {
+        return null;
+    }
+
+    @Override
+    public EmployeeDto update() {
+        return null;
+    }
+
+    @Override
+    public EmployeeDto getById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<EmployeeDto> getAll() {
+        return null;
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+}
