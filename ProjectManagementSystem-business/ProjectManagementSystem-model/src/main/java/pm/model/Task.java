5eb5cab08dfb59032c571a0ee4309552210efcb2
Tural Guluev <tural-98@bk.ru>
2023-05-13 17:07:20 +0300

    added fileds to Task class and TaskStatus enum

--- ProjectManagementSystem-business/ProjectManagementSystem-model/src/main/java/pm/model/Task.java
+++ ProjectManagementSystem-business/ProjectManagementSystem-model/src/main/java/pm/model/Task.java
@@ -1,4 +1,23 @@
 package pm.model;
 
+import com.digital.pm.common.enums.TaskStatus;
+
+import java.util.Date;
+
 public class Task {
+    private String name;
+    private String description;
+
+    private Employee executor;
+
+    private int laborCosts;
+
+    private Date deadline;
+    private TaskStatus status;
+
+    private String author;
+
+    private Date dateOfCreation;
+    private Date update;
+
 }
--- ProjectManagementSystem-common/src/main/java/com/digital/pm/common/enums/TaskStatus.java
+++ ProjectManagementSystem-common/src/main/java/com/digital/pm/common/enums/TaskStatus.java
@@ -1,4 +1,8 @@
 package com.digital.pm.common.enums;
 
 public enum TaskStatus {
+    NEW,
+    AT_WORK,
+    COMPLETED,
+    CLOSED
 }
