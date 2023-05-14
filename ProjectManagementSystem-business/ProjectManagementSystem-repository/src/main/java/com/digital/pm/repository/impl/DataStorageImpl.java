6d040c7464e3d377f0f67aa19335b9d5bdeeeb6e
Tural Guluev <tural-98@bk.ru>
2023-05-14 14:23:05 +0300

    added thread initialization for reading and writing to DataStorageImpl andd added resources package in repository module

--- ProjectManagementSystem-business/ProjectManagementSystem-repository/src/main/java/com/digital/pm/repository/impl/DataStorageImpl.java
+++ ProjectManagementSystem-business/ProjectManagementSystem-repository/src/main/java/com/digital/pm/repository/impl/DataStorageImpl.java
@@ -3,11 +3,34 @@ package com.digital.pm.repository.impl;
 import com.digital.pm.repository.DataStorage;
 import pm.model.Employee;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class DataStorageImpl implements DataStorage {
     private AtomicInteger atomicInteger = new AtomicInteger();
+    private String filePath = "src/main/resources/data.txt";
+    private FileReader fileReader;
+    private FileWriter fileWriter;
+
+    public DataStorageImpl() {
+        if (Files.notExists(Path.of(filePath))) {
+            try {
+                Files.createFile(Path.of(filePath));
+
+                fileReader = new FileReader(filePath);
+                fileWriter = new FileWriter(filePath);
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
     @Override
     public Employee create(Employee employee) {
