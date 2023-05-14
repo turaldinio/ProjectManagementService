8c20bef045bb53e54a9764b1e69a7871bda4e710
Tural Guluev <tural-98@bk.ru>
2023-05-14 19:01:29 +0300

    added logging of results to the console

--- ProjectManagementSystem-app/src/main/java/com/digital/pm/app/ApplicationConfig.java
+++ ProjectManagementSystem-app/src/main/java/com/digital/pm/app/ApplicationConfig.java
@@ -1,7 +1,10 @@
 package com.digital.pm.app;
 
 import com.digital.pm.dto.employee.CreateEmployeeDto;
+import com.digital.pm.dto.employee.EmployeeDto;
 import com.digital.pm.web.controller.EmployeeController;
+
+import java.util.List;
 
 public class ApplicationConfig {
     private static final EmployeeController employeeController = new EmployeeController();
@@ -37,28 +40,44 @@ public class ApplicationConfig {
                 email("ilushka93@mail.ru").
                 build();
 
-//        System.out.println("----------CREATE-------------");
-//
-//        var firstDto = employeeController.create(first);
-//        var secondDto = employeeController.create(second);
-//
-//        System.out.println(firstDto + " is created");
-//        System.out.println(secondDto + " is created");
-//
+        System.out.println("----------CREATE-------------");
+
+        var firstDto = employeeController.create(first);
+        var secondDto = employeeController.create(second);
+
+        System.out.println(firstDto + " is created");
+        System.out.println(secondDto + " is created");
 
 
-//        System.out.println("----------UPDATE-------------");
-//
-//        var thirdDto = employeeController.update(1, third);
-//        System.out.println(thirdDto + " is updated");
-//
+        System.out.println("----------UPDATE-------------");
+
+        var updateResult = employeeController.update(1, third);
+        printResult(updateResult);
+
+        System.out.println("----------getById-------------");
+
+        var getResult = employeeController.getById(2);
+        printResult(getResult);
+
+        System.out.println("----------deleteById-------------");
+
+        var deleteResult = employeeController.deleteById(1);
+        printResult(deleteResult);
+
+        System.out.println("----------getAll-------------");
+        printResult(employeeController.getAll());
 
 
-        //        System.out.println("----------GetById-------------");
 
-//        System.out.println("----------getById-------------");
-//        System.out.println(employeeController.getById(98));
+    }
 
-        System.out.println(employeeController.deleteById(2)+" has been deleted");
+    public static void printResult(EmployeeDto employeeDto) {
+        if (employeeDto != null) {
+            System.out.println(employeeDto);
+        }
+    }
+
+    public static void printResult(List<EmployeeDto> employeeDtoList) {
+        employeeDtoList.forEach(System.out::println);
     }
 }
