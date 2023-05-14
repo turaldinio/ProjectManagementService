e96629f8121eaf1fe00f258e027a7644daea9a7e
Tural Guluev <tural-98@bk.ru>
2023-05-14 20:03:52 +0300

    added ProjectDto fields

--- ProjectManagementSystem-dto/src/main/java/com/digital/pm/dto/ProjectDto.java
+++ ProjectManagementSystem-dto/src/main/java/com/digital/pm/dto/ProjectDto.java
@@ -1,4 +1,10 @@
 package com.digital.pm.dto;
 
+import com.digital.pm.common.enums.ProjectStatus;
+
 public class ProjectDto {
+    private int id;
+    private String name;
+    private String description;
+    private ProjectStatus status;
 }
