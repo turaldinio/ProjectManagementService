04485c186ed8c90b41836794977fe135aa4e56a1
Tural Guluev <tural-98@bk.ru>
2023-05-16 21:57:21 +0300

    added thr create method implementation

--- ProjectManagementSystem-business/ProjectManagementSystem-repository/src/main/java/com/digital/pm/repository/impl/DataBaseDataStorageImpl.java
+++ ProjectManagementSystem-business/ProjectManagementSystem-repository/src/main/java/com/digital/pm/repository/impl/DataBaseDataStorageImpl.java
@@ -1,21 +1,38 @@
 package com.digital.pm.repository.impl;
 
+import com.digital.pm.common.enums.EmployeeStatus;
 import com.digital.pm.model.Employee;
 import com.digital.pm.repository.DataStorage;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class DataBaseDataStorageImpl implements DataStorage {
     private Connection connection;
 
     public DataBaseDataStorageImpl(Connection connection) {
-        this.connection =connection;
+        this.connection = connection;
     }
 
     @Override
     public Employee create(Employee employee) {
+        try (var insert = connection.prepareStatement("insert into employee(first_name, last_name, patronymic, post, account, email, status_id)" +
+                "values (?,?,?,?,?,?,?)")) {
+            insert.setString(1, employee.getFirsName());
+            insert.setString(2, employee.getLastName());
+            insert.setString(3, employee.getPatronymic());
+            insert.setString(4, employee.getPost());
+            insert.setString(5, employee.getAccount());
+            insert.setString(6, employee.getEmail());
+            insert.setInt(7, employee.getStatus().ordinal());
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         return null;
     }
 
