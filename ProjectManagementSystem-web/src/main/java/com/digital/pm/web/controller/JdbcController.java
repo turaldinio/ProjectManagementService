eeb54f8f0e4a437222a4156cbf769ef5f4da2e6b
Tural Guluev <tural-98@bk.ru>
2023-05-16 19:47:11 +0300

    added getConnection method in Jdbc controller

--- ProjectManagementSystem-web/src/main/java/com/digital/pm/web/controller/JdbcController.java
+++ ProjectManagementSystem-web/src/main/java/com/digital/pm/web/controller/JdbcController.java
@@ -2,7 +2,9 @@ package com.digital.pm.web.controller;
 
 import lombok.Data;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @Data
 public class JdbcController {
@@ -10,6 +12,9 @@ public class JdbcController {
     private String user;
     private String password;
 
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url,user,password);
+    }
 
 
 
