bcfd10a462a268b6abb495b09c79bb2395a63d31
Tural Guluev <tural-98@bk.ru>
2023-05-16 19:46:29 +0300

    added JdbcController

---
+++ ProjectManagementSystem-web/src/main/java/com/digital/pm/web/controller/JdbcController.java
@@ -0,0 +1,16 @@
+package com.digital.pm.web.controller;
+
+import lombok.Data;
+
+import java.sql.DriverManager;
+
+@Data
+public class JdbcController {
+    private String url;
+    private String user;
+    private String password;
+
+
+
+
+}
