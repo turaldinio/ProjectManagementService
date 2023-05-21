b98c6319b6620ed4f0521d71f185a670bee0c8b3
Tural Guluev <tural-98@bk.ru>
2023-05-21 19:14:14 +0300

    added liqubase configuration file and dependences

--- ProjectManagementSystem-app/src/main/java/com/digital/pm/app/ProjectManagementSystemApplication.java
+++ ProjectManagementSystem-app/src/main/java/com/digital/pm/app/ProjectManagementSystemApplication.java
@@ -1,83 +1,63 @@
 package com.digital.pm.app;
 
-import com.digital.pm.dto.employee.CreateEmployeeDto;
 import com.digital.pm.dto.employee.EmployeeDto;
 import com.digital.pm.service.DataBaseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.List;
 
 @SpringBootApplication
-public class ProjectManagementSystemApplication implements CommandLineRunner {
+public class ProjectManagementSystemApplication {
     @Autowired
     private DataBaseService dataBaseService;
+    @Autowired
+    private DataBaseService dataBaseFileService;
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectManagementSystemApplication.class, args);
 
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        CreateEmployeeDto first = CreateEmployeeDto.
-                builder().
-                lastName("Романов").
-                firstName("Сергей").
-                patronymic("Иванович").
-                post("junior developer").
-                account("sergRom98").
-                email("romaniv@mail.ru").
-                build();
-
-        CreateEmployeeDto second = CreateEmployeeDto.
-                builder().
-                lastName("Иванов").
-                firstName("Евгений").
-                patronymic("Романович").
-                post("junior developer").
-                account("ivanka").
-                email("omtom@bk.ru").
-                build();
-
-        CreateEmployeeDto third = CreateEmployeeDto.
-                builder().
-                lastName("Лахимов").
-                firstName("Илья").
-                patronymic("Юрьевич").
-                post("junior developer").
-                account("kiOkr3").
-                email("laxiSu32@mail.ru").
-                build();
-
-
-        System.out.println("-------------CREATE------------");
-        printResult(dataBaseService.create(first));
-        printResult(dataBaseService.create(second));
+//    @Override
+//    public void run(String... args) throws Exception {
+//        CreateEmployeeDto first = CreateEmployeeDto.
+//                builder().
+//                lastName("Романов").
+//                firstName("Сергей").
+//                patronymic("Иванович").
+//                post("junior developer").
+//                account("sergRom98").
+//                email("romaniv@mail.ru").
+//                build();
 //
-//        System.out.println("-------------GETALL------------");
+//        CreateEmployeeDto second = CreateEmployeeDto.
+//                builder().
+//                lastName("Иванов").
+//                firstName("Евгений").
+//                patronymic("Романович").
+//                post("junior developer").
+//                account("ivanka").
+//                email("omtom@bk.ru").
+//                build();
 //
-//        printResult(employeeController.getAll());
+//        CreateEmployeeDto third = CreateEmployeeDto.
+//                builder().
+//                lastName("Лахимов").
+//                firstName("Илья").
+//                patronymic("Юрьевич").
+//                post("junior developer").
+//                account("kiOkr3").
+//                email("laxiSu32@mail.ru").
+//                build();
 //
 //
-//        System.out.println("-------------GETBYID------------");
+//        System.out.println("-------------CREATE------------");
+//        printResult(dataBaseService.create(first));
+//        printResult(dataBaseService.create(second));
 //
-//        printResult(employeeController.getById(2L));
-//
-//        System.out.println("-------------DELETE------------");
-//        printResult(employeeController.deleteById(1));
-//
-//        System.out.println("-------------UPDATE------------");
-//        printResult(employeeController.update(1L, third));
-//
-//        System.out.println("-------------FILTER------------");
-//        var filterEmployee = FilterEmployee.builder().firsName("Сергей").build();
-//
-//        printResult(employeeController.searchByFilter(filterEmployee));
-
-    }
+//    }
 
     public static void printResult(EmployeeDto employeeDto) {
         if (employeeDto != null) {
