833c0bed4288f4cd7c1d31ed31d84d8f90830bd0
Tural Guluev <tural-98@bk.ru>
2023-05-22 11:49:44 +0300

    added the ProjectFilter

---
+++ ProjectManagementSystem-common/src/main/java/com/digital/pm/common/filters/ProjectFilter.java
@@ -0,0 +1,10 @@
+package com.digital.pm.common.filters;
+
+import com.digital.pm.common.enums.ProjectStatus;
+
+public class ProjectFilter {
+    private Long id;
+    private String name;
+    private String description;
+    private ProjectStatus status;
+}
