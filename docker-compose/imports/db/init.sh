7d0652d6400bd1d5fe8995c70d9381232b45cdb9
Tural Guluev <tural-98@bk.ru>
2023-05-23 12:35:10 +0300

    update docker-compose.yaml (changed mysql->postgres)

--- docker-compose/imports/db/init.sh
+++ docker-compose/imports/db/init.sh
@@ -1,14 +1,14 @@
-##!/bin/bash
-#set -e
-#
-#echo "Creating databases..."
-#
-#
-#echo "Creating database itpm-service"
-#psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-#	CREATE DATABASE "lecture2-db";
-#EOSQL
-#
-#
-#
-#echo "Databases created!"
+#!/bin/bash
+set -e
+
+echo "Creating databases..."
+
+
+echo "Creating database itpm-service"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+	CREATE DATABASE "digital";
+EOSQL
+
+
+
+echo "Databases created!"
