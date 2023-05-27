4789620940a9ef6964bc7ececa2ca8e26e84e44a
Tural Guluev <tural-98@bk.ru>
2023-05-27 17:35:40 +0300

    added jwt dependences

--- ProjectManagementSystem-business/ProjectManagementSystem-service/src/main/java/com/digital/pm/service/impl/AuthorizationServiceImpl.java
+++ ProjectManagementSystem-business/ProjectManagementSystem-service/src/main/java/com/digital/pm/service/impl/AuthorizationServiceImpl.java
@@ -3,6 +3,9 @@ package com.digital.pm.service.impl;
 import com.digital.pm.common.auth.AuthorizationRequest;
 import com.digital.pm.service.AuthorizationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
