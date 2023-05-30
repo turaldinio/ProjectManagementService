02cb2dcf3490bb652c01de689b2b849c5cbd5c5f
Tural Guluev <tural-98@bk.ru>
2023-05-31 02:58:44 +0300

    move @ComponentScan annotation

--- ProjectManagementSystem-app/src/main/java/com/digital/pm/app/ApplicationConfig.java
+++ ProjectManagementSystem-app/src/main/java/com/digital/pm/app/ApplicationConfig.java
@@ -1,17 +1,11 @@
 package com.digital.pm.app;
 
-import com.digital.pm.service.mapping.EmployeeMapper;
-import com.digital.pm.service.mapping.ProjectMapper;
-import com.digital.pm.service.mapping.TaskMapper;
-import com.digital.pm.service.mapping.TeamMapper;
 import com.google.gson.Gson;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
-@ComponentScan("com.digital")
 public class ApplicationConfig {
     @Bean
     public Gson gson() {
--- ProjectManagementSystem-app/src/main/java/com/digital/pm/app/ProjectManagementSystemApplication.java
+++ ProjectManagementSystem-app/src/main/java/com/digital/pm/app/ProjectManagementSystemApplication.java
@@ -3,6 +3,7 @@ package com.digital.pm.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -15,6 +16,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
         @PropertySource("classpath:application.properties"),
         @PropertySource("classpath:security.properties")
 })
+@ComponentScan("com.digital")
+
 public class ProjectManagementSystemApplication  {
 
     public static void main(String[] args) {
