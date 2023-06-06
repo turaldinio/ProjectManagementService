93edf4d39b507453e82605320de3bbae7b85a5ec
Tural Guluev <tural-98@bk.ru>
2023-06-06 22:32:02 +0300

    added OpenApi security config

--- ProjectManagementSystem-business/ProjectManagementSystem-service/src/main/java/com/digital/pm/service/auth/impl/AuthorizationServiceImpl.java
+++ ProjectManagementSystem-business/ProjectManagementSystem-service/src/main/java/com/digital/pm/service/auth/impl/AuthorizationServiceImpl.java
@@ -10,6 +10,8 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+
 @Service
 @RequiredArgsConstructor
 public class AuthorizationServiceImpl implements AuthorizationService {
@@ -21,8 +23,7 @@ public class AuthorizationServiceImpl implements AuthorizationService {
     @Override
     public AuthorizationResponse authorize(AuthorizationRequest authorizationRequest) {
         var auth = authenticationManager.
-                authenticate(new UsernamePasswordAuthenticationToken(authorizationRequest.getLogin(), authorizationRequest.getPassword(),
-                        customUserDetailService.loadUserByUsername(authorizationRequest.getLogin()).getAuthorities()));
+                authenticate(new UsernamePasswordAuthenticationToken(authorizationRequest.getLogin(), authorizationRequest.getPassword()));
 
         SecurityContextHolder.getContext().setAuthentication(auth);
         var token = jwtService.generateToken(auth);
