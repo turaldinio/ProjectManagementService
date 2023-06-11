46ef7f5bf2f0459669cc3e55dd22ddcd509a0a36
Tural Guluev <tural-98@bk.ru>
2023-06-12 02:06:50 +0300

    created newtable task_file

--- ProjectManagementSystem-business/ProjectManagementSystem-model/src/main/java/com/digital/pm/model/TaskFile.java
+++ ProjectManagementSystem-business/ProjectManagementSystem-model/src/main/java/com/digital/pm/model/TaskFile.java
@@ -1,14 +1,13 @@
 package com.digital.pm.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "task_file")
 public class TaskFile {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "file_path")
     private String path;
 }
