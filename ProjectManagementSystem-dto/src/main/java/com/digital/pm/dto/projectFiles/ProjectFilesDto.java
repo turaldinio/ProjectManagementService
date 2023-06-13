61c0ac2e2e0958ff8c33e2f14a7725640b3c313d
Tural Guluev <tural-98@bk.ru>
2023-06-13 15:47:21 +0300

    added dto objects for ProjectFiles

---
+++ ProjectManagementSystem-dto/src/main/java/com/digital/pm/dto/projectFiles/ProjectFilesDto.java
@@ -0,0 +1,16 @@
+package com.digital.pm.dto.projectFiles;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ProjectFilesDto {
+    @Schema(description = "путь до файла")
+    private String filePath;
+}
