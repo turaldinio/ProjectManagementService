e9a2820bfdec10678d9da0d4add75995549f9578
Tural Guluev <tural-98@bk.ru>
2023-06-13 15:49:20 +0300

    added ProjectFileService

---
+++ ProjectManagementSystem-business/ProjectManagementSystem-service/src/main/java/com/digital/pm/service/ProjectFileService.java
@@ -0,0 +1,18 @@
+package com.digital.pm.service;
+
+import com.digital.pm.dto.projectFiles.CreateProjectFileDto;
+import com.digital.pm.dto.projectFiles.ProjectFilesDto;
+import com.digital.pm.model.ProjectFile;
+
+import java.io.File;
+import java.util.List;
+
+public interface ProjectFileService {
+    ProjectFilesDto saveFile(CreateProjectFileDto projectFileDto, Long projectId);
+
+    File downloadFile(Long projectId);
+
+    byte[] getFileBytes(File file);
+
+    ProjectFilesDto map(List<ProjectFile> files);
+}
